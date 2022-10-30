0fae433a02c48741c6d5175470eebff01da3bb76
Akhil Vinayak P <nirvanaosborn@gmail.com>
2022-10-30 18:20:38 +0530

    test: ui update

--- MusicApp/app.js
+++ MusicApp/app.js
@@ -1,1 +1,5 @@
-
+let song_urls = [
+    "https://dl.dropboxusercontent.com/s/ru7gps774eqwfzh/Elsei%20-%20Dreamer%20Radio%20%282%29.mp3?dl=0",
+    "https://dl.dropboxusercontent.com/s/n9vwho3qekfmuwt/4.%20cxlt.%20-%20Far%20Away%20.mp3?dl=0",
+    "https://dl.dropboxusercontent.com/s/877yjaidp4tpx8j/Lost%20Files%20-%20Behind%20The%20Hill.mp3?dl=0"
+]
